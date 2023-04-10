e6bebd4f23ebcbdb3d2707bd9bf7a55abdbacaf3
Rhi <renii@reniivali.com>
2023-04-10 15:36:10 -0600

    this is cursed

--- phys.js
+++ phys.js
@@ -38,10 +38,148 @@ function addBox(x, y, w, h, b, m, content, static) {
 	}
 }
 
+let iFlexDemoBoxes = 4;
+
+function unsetBasis() {
+	for (let i = 0; i < iFlexDemoBoxes - 1; i++) {
+		const flexDemoBox = d.getElementsByClassName("flexDemo")[i];
+		flexDemoBox.style.flexBasis = "";
+		d.getElementById(`flexBasisDemoVal${i}`).innerHTML = `Flex Basis: Unset`;
+		d.getElementById(`flexBasisDemoSlide${i}`).value = "1";
+	}
+}
+
+function unsetWidth() {
+	for (let i = 0; i < iFlexDemoBoxes - 1; i++) {
+		const flexDemoBox = d.getElementsByClassName("flexDemo")[i];
+		flexDemoBox.style.width = "";
+		d.getElementById(`flexRawWidthDemoVal${i}`).innerHTML = `Raw Width: Unset`;
+		d.getElementById(`flexRawWidthDemoSlide${i}`).value = "1";
+	}
+}
+
+/**
+ * @param min Slider's minimum value
+ * @param max Slider's maximum value
+ * @param step Slider's step value
+ * @param value Slider's starting value
+ * @param id Slider's base id
+ * @param did What the display p element should call the changing value
+ * @param dval What the display p element should display as the starting value
+ * @param cngval What the slider should change (CSS property in CamelCase)
+ * @param nid the number of the demo box
+ * @param appendTo the demo box element passed in
+ * @param change the ID of the element to change
+ * @param unit (OPTIONAL) the unit of the value (px, %, etc.)
+ */
+function sliderSetup(min, max, step, value, id, did, dval, cngval, nid, appendTo, change, unit) {
+	const container = d.createElement("div");
+	const disp = d.createElement("p");
+	disp.id = `${id}DemoVal${nid}`;
+	disp.innerHTML = `${did}: ${dval}`
+
+	const slider = d.createElement("input");
+	slider.type = "range";
+	slider.min = min;
+	slider.max = max;
+	slider.value = value;
+	slider.step = step;
+	slider.className = "slider";
+	slider.id = `${id}DemoSlide${nid}`;
+
+	appendTo.append(container);
+	container.append(disp, slider);
+	if (change) {
+		slider.addEventListener('input', function() {
+			appendTo.style[cngval] = slider.value + unit;
+			disp.innerHTML = `${did}: ${slider.value}${unit}`;
+		});
+	} else {
+		slider.addEventListener('input', function() {
+			disp.innerHTML = `${did}: ${slider.value}${unit}`;
+		});
+	}
+}
+
+function addFlexDemoBox() {
+	const flexDemoContainer = d.getElementById("flexDemoContainer");
+	const flexDemoBox = d.createElement("div");
+	flexDemoBox.className = "demo flexDemo";
+	flexDemoBox.id = `flexDemo${iFlexDemoBoxes + 1}`;
+	flexDemoBox.innerHTML = `<h1>Box ${iFlexDemoBoxes + 1}</h1>`;
+	flexDemoContainer.append(flexDemoBox);
+	iFlexDemoBoxes++;
+	sliderSetup(0, 1000, 1, 1, "flexBasis", "Flex Basis", "Unset", "flexBasis", iFlexDemoBoxes, flexDemoBox, true, "px");
+	sliderSetup(0, 1000, 1, 1, "flexRawWidth", "Raw Width", "Unset", "width", iFlexDemoBoxes, flexDemoBox, true, "px");
+	sliderSetup(0, 1, 0.001, 1, "flexGrow", "Flex Grow", "1", "flexGrow", iFlexDemoBoxes, flexDemoBox, true, "");
+	sliderSetup(0, 1, 0.001, 1, "flexShrink", "Flex Shrink", "1", "flexShrink", iFlexDemoBoxes, flexDemoBox, true, "");
+}
+
+function removeFlexDemoBox() {
+	if (iFlexDemoBoxes > 4) {
+		d.getElementById(`flexDemo${iFlexDemoBoxes}`).remove();
+		iFlexDemoBoxes--;
+	} else {
+		alert("You can't remove any more boxes!");
+	}
+}
+
 d.addEventListener('DOMContentLoaded', () => {
-	addBox(150, 150, 300, 300, 0.5, 1, "<div><h1>this is a div and it has physics</h1></div>", false);
-	addBox(500, 150, 300, 300, 0.5, 1, "<div><h1>this is another div and it has physics</h1></div>", false);
-	addBox(1000, 150, 600, 600, 0.5, 1, "<div><h1>i wanted to make this website a pain to use</h1><button>it's a button</button></div>");
+	addBox(150, 150, 300, 300, 0.5, 1, "<div class=\"subSection\">\n\t\t\t\t<h1>Border Radius</h1>\n\t\t\t\t<div id=\"borderRadDemo\"></div>\n\t\t\t\t<p id=\"borderRadDemoVal\" style=\"margin-bottom: 0;\">Radius: 5px</p>\n\t\t\t\t<input type=\"range\" min=\"0\" max=\"100\" value=\"5\" class=\"slider\" id=\"borderRadDemoSlide\">\n\t\t\t</div>", false);
+	addBox(500, 150, 300, 300, 0.5, 1, "<div class=\"subSection\" id=\"boxShadowDemoContainer\">\n" +
+		"<h1>Box Shadow</h1>\n" +
+		"<div id=\"boxShadowDemo\"></div>\n" +
+		"<div id=\"boxShadowDemoSliders\"></div>\n" +
+		"</div>", false);
+	addBox(550, 900, 1000, 600, 0.5, 1, "<div class=\"subSection\" style=\"flex: 85%\">\n" +
+		"<h1>Flexbox</h1>\n" +
+		"<div id=\"flexDemoContainer\">\n" +
+		"<div class=\"demo flexDemo\">\n" +
+		"<h1>Box 1</h1>\n" +
+		"</div>\n" +
+		"<div class=\"demo flexDemo\">\n" +
+		"<h1>Box 2</h1>\n" +
+		"</div>\n" +
+		"<div class=\"demo flexDemo\">\n" +
+		"<h1>Box 3</h1>\n" +
+		"</div>\n" +
+		"<div class=\"demo flexDemo\">\n" +
+		"<h1>Box 4</h1>\n" +
+		"</div>\n" +
+		"</div>\n" +
+		"<h2 style=\"margin: 0;\">Container Properties</h2>\n" +
+		"<div class=\"flexContainer\">\n" +
+		"<div class=\"demo flexBox\" style=\"width: 300px;\">\n" +
+		"<h2>Direction</h2>\n" +
+		"<select id=\"flexDirection\">\n" +
+		"<option value=\"row\">Row</option>\n" +
+		"<option value=\"row-reverse\">Row Reverse</option>\n" +
+		"<option value=\"column\">Column</option>\n" +
+		"<option value=\"column-reverse\">Column Reverse</option>\n" +
+		"</select>\n" +
+		"</div>\n" +
+		"<div class=\"demo flexBox\" style=\"width: 160px\">\n" +
+		"<h2>Wrap</h2>\n" +
+		"<select id=\"flexWrap\">\n" +
+		"<option value=\"nowrap\">No Wrap</option>\n" +
+		"<option value=\"wrap\">Wrap</option>\n" +
+		"<option value=\"wrap-reverse\">Wrap Reverse</option>\n" +
+		"</select>\n" +
+		"</div>\n" +
+		"<div class=\"demo flexBox\">\n" +
+		"<h2>Basis (all boxes)</h2>\n" +
+		"<button onclick=\"unsetBasis()\">Unset</button>\n" +
+		"</div>\n" +
+		"<div class=\"demo flexBox\">\n" +
+		"<h2>Raw Width (all boxes)</h2>\n" +
+		"<button onclick=\"unsetWidth()\">Unset</button>\n" +
+		"</div>\n" +
+		"<div class=\"demo flexBox\">\n" +
+		"<h2>Add / Remove Demo Boxes</h2>\n" +
+		"<button onclick=\"addFlexDemoBox()\">Add Box</button>\n" +
+		"<button onclick=\"removeFlexDemoBox()\">Remove Box</button>\n" +
+		"</div>\n" +
+		"</div>");
 
 	addBox(w.innerWidth / 2, w.innerHeight + 50, w.innerWidth, 100, 0.5, 1, "", true);
 	addBox(w.innerWidth / 2, -50, w.innerWidth, 100, 0.5, 1, "", true);
@@ -75,4 +213,45 @@ d.addEventListener('DOMContentLoaded', () => {
 	w.addEventListener('mousedown', (e) => {
 		explode = {d: true, x: e.clientX, y: e.clientY};
 	});
+
+	//css dict
+	const borderRadSlider = d.getElementById("borderRadDemoSlide")
+	const borderRadElement = d.getElementById("borderRadDemo")
+	borderRadSlider.addEventListener('input', function() {
+		borderRadElement.style.borderRadius = borderRadSlider.value + "px";
+		d.getElementById("borderRadDemoVal").innerHTML = `Radius: ${borderRadSlider.value}px`;
+	});
+
+	const flexDemoBoxes = d.getElementsByClassName("flexDemo");
+	for (let i = 0; i < 4; i++) {
+		sliderSetup(0, 1000, 1, 1, "flexBasis", "Flex Basis", "Unset", "flexBasis", i, flexDemoBoxes[i], true, "px");
+		sliderSetup(0, 1000, 1, 1, "flexRawWidth", "Raw Width", "Unset", "width", i, flexDemoBoxes[i], true, "px");
+		sliderSetup(0, 1, 0.001, 1, "flexGrow", "Flex Grow", "1", "flexGrow", i, flexDemoBoxes[i], true, "");
+		sliderSetup(0, 1, 0.001, 1, "flexShrink", "Flex Shrink", "1", "flexShrink", i, flexDemoBoxes[i], true, "");
+	}
+
+	d.getElementById("flexDirection").addEventListener('change', function() {
+		d.getElementById("flexDemoContainer").style.flexDirection = d.getElementById("flexDirection").value;
+	});
+
+	d.getElementById("flexWrap").addEventListener('change', function() {
+		d.getElementById("flexDemoContainer").style.flexWrap = d.getElementById("flexWrap").value;
+	});
+
+	const boxShadowContainer = d.getElementById("boxShadowDemoSliders");
+	const boxShadowDemo = d.getElementById("boxShadowDemo");
+	sliderSetup(0, 100, 1, 0, "boxShadowX", "offset-x", "0", "", 1, boxShadowContainer, false,  "px");
+	sliderSetup(0, 100, 1, 0, "boxShadowY", "offset-y", "0", "", 1, boxShadowContainer, false,  "px");
+	sliderSetup(0, 100, 1, 0, "boxShadowBlur", "blur-radius", "0", "", 1, boxShadowContainer, false,  "px");
+	sliderSetup(0, 100, 1, 0, "boxShadowSpread", "spread-radius", "0", "", 1, boxShadowContainer, false,  "px");
+
+	function updateBoxShadow() {
+		const x = d.getElementById("boxShadowXDemoSlide1").value;
+		const y = d.getElementById("boxShadowYDemoSlide1").value;
+		const blur = d.getElementById("boxShadowBlurDemoSlide1").value;
+		const spread = d.getElementById("boxShadowSpreadDemoSlide1").value;
+		boxShadowDemo.style.boxShadow = `${x}px ${y}px ${blur}px ${spread}px black`;
+	}
+
+	setInterval(updateBoxShadow, 50);
 });
